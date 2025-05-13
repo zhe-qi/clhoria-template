b6a77084ca336628f48439265cafce1349bc3ef0
zhe-qi <zheqi0910@gmail.com>
2025-05-13 23:13:38 +0800

    feat: generate db migrate.

---
+++ src/db/migrations/0000_aberrant_stingray.sql
@@ -0,0 +1,73 @@
+CREATE TYPE "public"."type" AS ENUM('dir', 'menu', 'button');--> statement-breakpoint
+CREATE TABLE "admin_users" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"password" text NOT NULL,
+	"role_id" varchar(64),
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	CONSTRAINT "admin_users_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+CREATE TABLE "casbin_rule" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"ptype" varchar(254),
+	"v0" varchar(254),
+	"v1" varchar(254),
+	"v2" varchar(254),
+	"v3" varchar(254),
+	"v4" varchar(254),
+	"v5" varchar(254)
+);
+--> statement-breakpoint
+CREATE TABLE "client_users" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"password" text NOT NULL,
+	"role" text DEFAULT 'user' NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL,
+	CONSTRAINT "client_users_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+CREATE TABLE "menu" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"path" varchar(100) NOT NULL,
+	"name" varchar(50) NOT NULL,
+	"type" "type" NOT NULL,
+	"parent_id" uuid,
+	"method" varchar(10) DEFAULT '',
+	"icon" varchar(50),
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "roles" (
+	"id" varchar(64) PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"status" integer DEFAULT 1,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "tasks" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"done" boolean DEFAULT false NOT NULL,
+	"created_at" timestamp NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user_roles" (
+	"user_id" uuid,
+	"role_id" varchar(64),
+	CONSTRAINT "user_roles_user_id_role_id_pk" PRIMARY KEY("user_id","role_id")
+);
+--> statement-breakpoint
+ALTER TABLE "admin_users" ADD CONSTRAINT "admin_users_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "casbin_rule" ADD CONSTRAINT "casbin_rule_v0_roles_id_fk" FOREIGN KEY ("v0") REFERENCES "public"."roles"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_admin_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."admin_users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+CREATE INDEX "ptype_index" ON "casbin_rule" USING btree ("ptype");--> statement-breakpoint
+CREATE INDEX "status_index" ON "roles" USING btree ("status");
