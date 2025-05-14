42666138c9256dc2211a64a19d5c42d2c5ec0c44
zhe-qi <zheqi0910@gmail.com>
2025-05-14 12:48:06 +0800

    feat: 更新路径规则

--- src/db/schema/casbin-rule.ts
+++ src/db/schema/casbin-rule.ts
@@ -7,9 +7,9 @@ export const casbinTable = pgTable("casbin_rule", {
   id: defaultColumns.id,
   // 策略类型：p（策略）/g（角色继承）
   ptype: varchar("ptype", { length: 254 }),
-  // 关联角色
+  // 关联角色 或者 继承角色
   v0: varchar({ length: 254 }),
-  // 资源路径（对应 `menu.path`）
+  // 资源路径（对应 `menu.path`）或者 被继承角色
   v1: varchar({ length: 254 }),
   // 操作类型（对应 `menu.method` 或自定义动作）
   v2: varchar({ length: 254 }),
--- src/lib/casbin/index.ts
+++ src/lib/casbin/index.ts
@@ -1,6 +1,15 @@
 import { newEnforcer } from "casbin";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 import { DrizzleCasbinAdapter } from "./adapter";
 
 const adapter = await DrizzleCasbinAdapter.newAdapter();
-export const enforcerLaunchedPromise = newEnforcer("src/lib/casbin/model.conf", adapter);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const enforcerLaunchedPromise = newEnforcer(
+  path.resolve(__dirname, "model.conf"),
+  adapter,
+);
