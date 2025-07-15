586aba4cc551c97b9d9e3aea7a387238da34a166
zhe-qi <zheqi0910@gmail.com>
2025-07-15 09:49:52 +0800

    feat: 更新

--- src/db/index.ts
+++ src/db/index.ts
@@ -5,12 +5,20 @@ import env from "@/env";
 
 import * as schema from "./schema";
 
-const queryClient = postgres(env.DATABASE_URL);
+const queryClient = postgres(env.DATABASE_URL, {
+  max: 10, // 默认最大连接数=10
+  idle_timeout: 10, // 空闲连接保留10秒
+  connect_timeout: 30, // 连接超时30秒
+  transform: {
+    undefined: null, // 处理JS undefined转SQL NULL
+  },
+});
 
 const db = drizzle({
   client: queryClient,
   schema,
   casing: "snake_case",
+  logger: env.NODE_ENV !== "production", // 开发环境日志
 });
 
 export default db;
