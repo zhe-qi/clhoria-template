4a128851327ec7706f0bac3190927397cec01686
zhe-qi <zheqi0910@gmail.com>
2025-07-21 15:13:50 +0800

    feat: 更新目录结构

--- src/routes/admin/admin.index.ts
+++ src/routes/admin/admin.index.ts
@@ -1,11 +1,5 @@
 // 后台管理，需要认证和鉴权的路由组
 export * from "./api-keys/api-keys.index";
-export * from "./authorization/authorization.index";
 export * from "./login-log/login-log.index";
 export * from "./operation-log/operation-log.index";
-export * from "./sys-access-keys/sys-access-keys.index";
-export * from "./sys-domains/sys-domains.index";
-export * from "./sys-endpoints/sys-endpoints.index";
-export * from "./sys-menus/sys-menus.index";
-export * from "./sys-roles/sys-roles.index";
-export * from "./sys-users/sys-users.index";
+export * from "./system";
---
+++ src/routes/admin/system/index.ts
@@ -0,0 +1,7 @@
+export * from "./authorization/authorization.index";
+export * from "./sys-access-keys/sys-access-keys.index";
+export * from "./sys-domains/sys-domains.index";
+export * from "./sys-endpoints/sys-endpoints.index";
+export * from "./sys-menus/sys-menus.index";
+export * from "./sys-roles/sys-roles.index";
+export * from "./sys-users/sys-users.index";
